374c8a33e28178ff71abcb271fd9dfdf4f18f70f
zjmart1 <zjmart1@ilstu.edu>
2021-03-07 16:27:08 -0600

    multsound

--- Unit2/multSound/sketch.js
+++ Unit2/multSound/sketch.js
@@ -1,36 +1,67 @@
-let sNotPressed, sPressed;
+let state = 0;
+let song1, song2, song3 ;
 
 function preload() {
-  sNotPressed = loadSound("assets/California.mp3");
-  sPressed = loadSound("assets/wheredreamscometrue.mp3");
+  song1 = loadSound("assets/brave.mp3") ;
+  song1 = loadSound("assets/hopeisreal.mp3") ;
+  song1 = loadSound("assets/wild.mp3") ;
 
+  song1.loop() ;
+  song1.pause() ;
+  song2.loop() ;
+  song2.pause() ;
+  song3.loop() ;
+  song3.pause() ;
 }
 
 function setup() {
-  createCanvas(500, 500);
-  sNotPressed.loop();
+  createCanvas(400, 400);
+
 
 }
 
 function draw() {
+
   background(100);
 
-  if (!(mouseIsPressed) && (sPressed.isPlaying())) {
-    sPressed.pause();
-    sNotPressed.loop();
-    print("triggering Non-Pressed song");
+  switch (state) {
 
-  } else if ((mouseIsPressed) && (sNotPressed.isPlaying())) {
-    sNotPressed.pause();
-    sPressed.loop();
-    print("triggering Pressed song");
+    case 0:
+      song1. play() ;
+      state = 1 ;
+      break;
+
+    case 1:
+      text("listen to song 1", 100, 100);
+      break;
+
+    case 2:
+      song2. play();
+      state = 3 ;
+      break;
+
+    case 3:
+      text("listen to song 2", 100, 100);
+      break;
+
+    case 4:
+      song3. play();
+      state = 5 ;
+      break;
+
+    case 5:
+      text("listen to song 3" , 100 , 100);
+      break;
 
   }
-
-
 }
 
-// add these to programs that use sound, at the bottom
-function touchStarted() {
-  getAudioContext().resume();
+function mouseReleased() {
+  song1.pause() ;
+  song2.pause() ;
+  song3.pause() ;
+
+  state++;
+  if (state > 5) state = 0;
+
 }
