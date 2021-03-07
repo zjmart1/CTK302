6dd138c0a727742b0dcd4e4bbf5c57fd86a3cb9f
zjmart1 <zjmart1@ilstu.edu>
2021-03-07 16:48:09 -0600

    joke1

--- Unit2/joke1/sketch.js
+++ Unit2/joke1/sketch.js
@@ -2,27 +2,30 @@ let state = 0 ;
 
 function setup() {
   createCanvas(500, 500);
-  textAlign(CENTER)
+  textAlign(CENTER) ;
+  textSize(24);
 }
 
 function draw() {
 
   switch(state) {
+
     case 0:
     background('grey') ;
-    text("Hear about the new restaurant called Karma?", width/2, height/2)
+    text("Hear about the new restaurant called Karma?", width/2, height/2);
     break;
 
     case 1:
     background('yellow') ;
-    text("There’s no menu: You get what you deserve.", width/2, height/2)
+    text("There’s no menu: You get what you deserve.", width/2, height/2);
     break;
   }
 
 }
 
 function mouseReleased(){
-  state++ ;
-  if (state > 1);
+  state  =  state + 1 ;
+  if (state > 1) {
     state = 0 ;
+}
 }
