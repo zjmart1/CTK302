71cf417b834af63f23c5ebba0a4df55e9eb0e822
zjmart1 <zjmart1@ilstu.edu>
2021-03-07 17:29:10 -0600

    joke2

--- Unit2/joke2/sketch.js
+++ Unit2/joke2/sketch.js
@@ -4,7 +4,7 @@ let timer = 0 ;
 function setup() {
   createCanvas(500, 500);
   textAlign(CENTER);
-  textSize(24);
+  textSize(20);
 }
 
 function draw() {
@@ -12,20 +12,21 @@ function draw() {
   switch(state) {
     case 0:
     background('grey') ;
-    text("What did the microwave say to the other microwave?", width/2) ;
+    text("What did the microwave say to the other microwave?", width/2, height/2) ;
     break;
 
     case 1:
     background('yellow') ;
-    text("Is it just me, or is it hot in here?", width/2) ;
+    text("Is it just me, or is it hot in here?", width/2, height/2) ;
     break;
   }
 
   timer++ ;
-  if (timer > 3 * 60) ;
+  if (timer > 3 * 60) { ;
     state++ ;
+    timer = 0 ;
     if (state > 1) {
       state = 0 ;
-      timer = 0 ;
+    }
     }
 }
