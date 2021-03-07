8d983315b837befb9c9958bf47a709a6eda88ac5
zjmart1 <zjmart1@ilstu.edu>
2021-03-07 14:32:46 -0600

    state

--- Unit2/state/sketch.js
+++ Unit2/state/sketch.js
@@ -1,4 +1,4 @@
-let state = 0 ;
+let state = 0;
 
 
 function setup() {
@@ -6,44 +6,77 @@ function setup() {
 }
 
 function draw() {
-  switch(state) {
+  switch (state) {
 
-    case 0 :
-    background(100) ;
-    text("state 0", 100, 100) ;
-    for (var j = 0; j <= 400; j++) {
-    for (var i = 0; i <= 400; i++) {
-      fill(0, random(255), 0);
-      rect(i * 40, j * 40, 50, 10);
-    break ;
+    case 0:
+      background(100);
+      text("state 0", 100, 100);
+
+      for (let j = 0; j <= 400; j++) {
+        for (let i = 0; i <= 400; i++) {
+          fill(0, random(255), 0);
+          rect(i * 40, j * 40, 50, 10);
+        }
+      }
+          break;
 
 
-    case 1 : 
-    background('red') ;
-    text("state 1", 100, 100) ;
-    break ;
-}
-    case 2 :
-    background('blue') ;
-    text("state 2", 100, 100) ;
-    break ;
+          case 1:
+            background('red');
+          text("state 1", 100, 100);
 
-    case 3 :
-    background('green') ;
-    text("state 3", 100, 100) ;
-    break ;
+          for (let j = 0; j <= 400; j++) {
+            for (let i = 0; i <= 400; i++) {
+              fill(200, 0, 0);
+              rect(i * 40, j * 40, 50, 10);
+            }
+          }
+          break;
 
-    case 4 :
-    background('silver') ;
-    text("state  4", 100, 100) ;
-    break ;
+        case 2:
+          background('blue');
+        text("state 2", 100, 100);
 
+        for (let j = 0; j <= 400; j++) {
+          for (let i = 0; i <= 400; i++) {
+            fill(0, 0, 200);
+            rect(i * 40, j * 40, 50, 10);
+          }
+        }
+        break;
+
+        case 3:
+          background('green');
+        text("state 3", 100, 100);
+
+        for (let j = 0; j <= 400; j++) {
+          for (let i = 0; i <= 400; i++) {
+            fill(0, 0, random(255));
+            rect(i * 40, j * 40, 50, 10);
+          }
+        }
+
+        break;
+
+        case 4:
+          background('silver');
+        text("state  4", 100, 100);
+
+        for (let j = 0; j <= 400; j++) {
+          for (let i = 0; i <= 400; i++) {
+            fill(208, 118, 199);
+            rect(i * 40, j * 40, 50, 10);
+          }
+        }
+
+        break;
+
+      }
   }
-}
 
-function mouseReleased() {
-  state = state + 1;
-  if (state > 2) {
-    state = 0 ;
+  function mouseReleased() {
+    state = state + 1;
+    if (state > 4) {
+      state = 0;
+    }
   }
-}
